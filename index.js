e857f4dd2346993c0591069efc46b85ee51dec8d
lncrlsbrt <liancarlosmbarte@gmail.com>
2025-11-07 14:28:15 +0800

    index.js with eq and db

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // import module `express`
 const express = require('express');
@@ -10,24 +9,11 @@ const hbs = require('hbs');
 const routes = require('./routes/routes.js');
 
 // import module `database` from `./model/db.js`
-=======
-// import module express
-const express = require('express');
-
-// import module hbs
-const hbs = require('hbs');
-
-// import module routes from ./routes/routes.js
-const routes = require('./routes/routes.js');
-
-// import module database from ./model/db.js
->>>>>>> b85b0d0a538335b17fa3c9ef92037f556bce56f7
 const db = require('./models/db.js');
 
 const app = express();
 const port = 9090;
 
-<<<<<<< HEAD
 // set `hbs` as view engine
 app.set('view engine', 'hbs');
 
@@ -37,18 +23,11 @@ hbs.registerHelper('eq', function (a, b) {
 });
 
 // sets `/views/partials` as folder containing partial hbs files
-=======
-// set hbs as view engine
-app.set('view engine', 'hbs');
-
-// sets /views/partials as folder containing partial hbs files
->>>>>>> b85b0d0a538335b17fa3c9ef92037f556bce56f7
 hbs.registerPartials(__dirname + '/views/partials');
 
 // parses incoming requests with urlencoded payloads
 app.use(express.urlencoded({extended: true}));
 
-<<<<<<< HEAD
 // set the folder `public` as folder containing static assets
 // such as css, js, and image files
 app.use(express.static('public'));
@@ -57,33 +36,15 @@ app.use(express.static('public'));
 app.use('/', routes);
 
 // if the route is not defined in the server, render `../views/error.hbs`
-=======
-// set the folder public as folder containing static assets
-// such as css, js, and image files
-app.use(express.static('public'));
-
-// define the paths contained in ./routes/routes.js
-app.use('/', routes);
-
-// if the route is not defined in the server, render ../views/error.hbs
->>>>>>> b85b0d0a538335b17fa3c9ef92037f556bce56f7
 // always define this as the last middleware
 app.use(function (req, res) {
     res.render('error');
 });
 
 // connects to the database
-<<<<<<< HEAD
-//db.connect();
-=======
 db.connect();
->>>>>>> b85b0d0a538335b17fa3c9ef92037f556bce56f7
 
 // binds the server to a specific port
 app.listen(port, function () {
     console.log('app listening at port ' + port);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> b85b0d0a538335b17fa3c9ef92037f556bce56f7
