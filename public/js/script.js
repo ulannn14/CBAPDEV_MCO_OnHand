c3ac7e1d303ae918f80a058feaff8289c5b18ef0
lelepods <leighalbo23@gmail.com>
2025-11-10 22:31:16 +0800

    chatbox hbs

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import exphbs from 'express-handlebars';
+import path from 'path';
+const app = express();
+
+app.engine('hbs', exphbs.engine({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: path.join(process.cwd(), 'views/layouts'),
+  partialsDir: path.join(process.cwd(), 'views/partials'),
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+// static files
+app.use(express.static('public'));
+
+// routes
+app.get('/messages', (req, res) => {
+  res.render('pages/chatbox-customer', { title: 'Messages' });
+});
+
+app.get('/messages/provider', (req, res) => {
+  res.render('pages/chatbox-provider', { title: 'Messages' });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
