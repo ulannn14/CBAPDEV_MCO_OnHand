6121b4b69d818119a6ee19c3f2d6e6b5169e2b6c
lelepods <leighalbo23@gmail.com>
2025-11-26 15:29:25 +0800

    fixed user logged in for messages and bookings page + chatbox compose area

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -6,10 +6,19 @@ const db = require('../models/db.js');
 const bookingController = {
 
     getBookings: async function (req, res) {
+    try {
+      const loggedInUser = req.session.user;
+      if (!loggedInUser) return res.redirect('/');
 
-        res.render('bookings');
+      res.render('bookings', {
+        user: loggedInUser
+      });
 
+    } catch (err) {
+      console.error('Error in getMessage:', err);
+      res.status(500).send('Internal Server Error');
     }
+  }
 }
 
 module.exports = bookingController;
--- controllers/messageController.js
+++ controllers/messageController.js
@@ -6,10 +6,20 @@ const db = require('../models/db.js');
 const messageController = {
 
     getMessages: async function (req, res) {
+    try {
+      const loggedInUser = req.session.user;
+      if (!loggedInUser) return res.redirect('/');
 
-        res.render('chatbox');
+      res.render('chatbox', {
+        user: loggedInUser
+      });
 
+    } catch (err) {
+      console.error('Error in getMessage:', err);
+      res.status(500).send('Internal Server Error');
     }
+  }
+  
 }
 
 module.exports = messageController;
