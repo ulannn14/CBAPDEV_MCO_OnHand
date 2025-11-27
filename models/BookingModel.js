47cc735a5c4911c7b42162621e12be8e6b422866
lncrlsbrt <liancarlosmbarte@gmail.com>
2025-11-27 21:06:17 +0800

    booking page rating debug

--- models/BookingModel.js
+++ models/BookingModel.js
@@ -41,7 +41,7 @@ const bookingSchema = new mongoose.Schema({
   // Booking progress status
   status: {
     type: String,
-    enum: ['Ongoing', 'ToRate', 'Done'],
+    enum: ['Ongoing', 'ToRate', 'Completed'],
     default: 'Ongoing'
   },
 
